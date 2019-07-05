6e4070603ecfa0792437f0e700a21fde75da5e7c
olugboyegaonimole <39015008+olugboyegaonimole@users.noreply.github.com>
2019-07-05 15:35:51 +0100

    Add files via upload

---
+++ process1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello Process!</title>
+</head>
+<body>
+
+<?php
+
+	$name = $_POST["name"];
+
+	echo "Hello, " . $name;
+
+
+?>
+
+
+
+
+
+
+
+</body>
+</html>
